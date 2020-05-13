d67568992ecca024216b5dcbda74a9830f83aaf0
Indunil1992 <indunil@adroitlogic.com>
2020-05-13 19:49:32 +0530

    S-Balance

--- test726indu-may13.js
+++ test726indu-may13.js
@@ -1,4 +1,18 @@
+let Swagger = require('swagger-client');
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let response = await Swagger.http({
+            url: `https://api-sandbox.starlingbank.com/api/v2/accounts/123/balance`,
+            method: 'get',
+            query: {},
+            headers: { "Accept": "application/json" }
+        });
+        // your code goes here
+
+    } catch (err) {
+        // error handling goes here
+    }
+
+    return { "message": "Successfully executed 1" };
 };
